77ad3b123e154dcccb385521677eae3b33256670
claralius <77200160+claralius@users.noreply.github.com>
2022-11-10 12:48:32 +0700

    Delete resources directory

--- resources/views/home.blade.php
+++
@@ -1,8 +0,0 @@
-<head>
-    @include('template')
-</head>
-
-    <h1>Books and Movies Website</h1>
-    <h3><a href="/books">Click here to view books</a></h3>
-    <h3><a href="/movies">Click here to view movies</a></h3>
-
